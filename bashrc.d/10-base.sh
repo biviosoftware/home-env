a161155c498961c9b4b6fe5bf9c1bcb89fdecd30
robnagler <github@q33.us>
2014-11-30 15:36:10 +0000

    psqlrc added; b_pyenv added

--- bashrc.d/10-base.sh
+++ bashrc.d/10-base.sh
@@ -1,91 +1,7 @@
 # -*-sh-*-
-if [ $(expr match "$BASH_SOURCE" ~/src) == 0 -a -d ~/src/biviosoftware/home-env ]; then
+if [ $(expr match "$BASH_SOURCE" ~/src) = 0 -a -d ~/src/biviosoftware/home-env ]; then
     # Execute user's dot files only
     return
-fi
-bivio_base_sh=true
-unset BCONF
-umask o-rwx
-export LOGNAME=${LOGNAME:-$(logname)}
-
-if [ ! -z "$PS1" -a "x$TERM" != xdumb ]; then
-    stty quit '^_'
-fi
-
-export CVSUMASK=07
-if test -z "$CVSROOT"; then
-    if test -d /home/cvs/CVSROOT; then
-	# We're on the CVS server
-	export CVSROOT=/home/cvs
-    elif test $(expr match "$(hostname)" '\(dfw4\|dfw1\|dfw3\|apa3\|apa11\|apa1\)\.'); then
-	# direct connect
-	export CVSROOT=":pserver:$LOGNAME@locker.bivio.biz:/home/cvs"
-    else
-	# We're remote and do SSH tunneling
-	export CVSROOT=":pserver:$LOGNAME@localhost:/home/cvs"
-    fi
-fi
-
-for f in \
-    $(ls -td /usr/java/{jdk*,jre*} /opt/IBMJava* 2>/dev/null) \
-    /opt/local/bin \
-    /usr/local/bin \
-    /usr/local/cuda/bin \
-    /usr/local/anaconda/bin \
-    $(test $UID = 0 && echo /sbin /usr/sbin /usr/local/sbin) \
-    ~/bin \
-    ; do
-    if [ -d $f -a $(expr match ":$PATH:" ".*:$f:") = 0 ]; then
-	export PATH="$f:$PATH"
-    fi
-done
-unset f
-
-if test $UID = 0 -o "$USER" = cvs; then
-    export CVSREAD=true
-fi
-
-if test $UID = 0; then
-    function bi {
-        local p=$1
-	shift
-        test -f /etc/$p.bconf && p=perl-app-$p
-        bivio release install $p "$@"
-    }
-    function bihs {
-        bivio release install_host_stream
-    }
-fi
-
-# Get most recent java and any jars in /usr/java
-for f in $(ls /usr/java/*.jar 2> /dev/null); do
-    export CLASSPATH=$CLASSPATH${CLASSPATH+:}$f
-done
-unset f
-
-if [ -d $HOME/src/java ]; then
-    export JAVA_ROOT=$HOME/src/java
-    export CLASSPATH="$CLASSPATH:$JAVA_ROOT"
-fi
-
-if [ -z "$PERLLIB" -a -d $HOME/src/perl ]; then
-    export PERLLIB=$HOME/src/perl
-fi
-export FTP_PASSIVE=1
-
-if test -f ~/.ssh/ssh_agent; then
-    . ~/.ssh/ssh_agent > /dev/null
-    if [ "$PS1" ]; then
-        if ps ${SSH_AGENT_PID-0} 2>&1 | grep ssh-agent > /dev/null 2>&1; then
-	    : We have a daemon
-	else
-	    # Start a daemon and add
-	    ssh-agent > ~/.ssh/ssh_agent
-	    . ~/.ssh/ssh_agent
-	    ssh-add
-            (x=~/.vagrant.d/insecure_private_key && test -f $x && ssh-add $x)
-	fi
-    fi
 fi
 
 # must be "function", because "dirs" may be an alias. "dirs () { " will throw
@@ -129,6 +45,18 @@ function ba {
     bivio test acceptance "${@-.}"
 }
 
+if test $UID = 0; then
+    function bi {
+        local p=$1
+	shift
+        test -f /etc/$p.bconf && p=perl-app-$p
+        bivio release install $p "$@"
+    }
+    function bihs {
+        bivio release install_host_stream
+    }
+fi
+
 function g {
     local x="$1"
     shift
@@ -149,6 +77,83 @@ function gp {
         "$x" "${@-.}" |
 	egrep -v '/old/|/files/artisans/plain/f/bOP|Util/t/Dev.tmp|/pkgs/(build|tmp)|Bivio/bOP.pm'
 }
+
+function b_path_insert {
+    local f="$1"
+    if [ -d $f -a $(expr match ":$PATH:" ".*:$f:") = 0 ]; then
+	export PATH="$f:$PATH"
+    fi
+}
+
+unset BCONF
+umask o-rwx
+export LOGNAME=${LOGNAME:-$(logname)}
+
+if [ ! -z "$PS1" -a "x$TERM" != xdumb ]; then
+    stty quit '^_'
+fi
+
+export CVSUMASK=07
+if test -z "$CVSROOT"; then
+    if test -d /home/cvs/CVSROOT; then
+	# We're on the CVS server
+	export CVSROOT=/home/cvs
+    elif test $(expr match "$(hostname)" '\(dfw4\|dfw1\|dfw3\|apa3\|apa11\|apa1\)\.'); then
+	# direct connect
+	export CVSROOT=":pserver:$LOGNAME@locker.bivio.biz:/home/cvs"
+    else
+	# We're remote and do SSH tunneling
+	export CVSROOT=":pserver:$LOGNAME@localhost:/home/cvs"
+    fi
+fi
+
+for f in \
+    $(ls -td /usr/java/{jdk*,jre*} /opt/IBMJava* 2>/dev/null) \
+    /opt/local/bin \
+    /usr/local/bin \
+    /usr/local/cuda/bin \
+    /usr/local/anaconda/bin \
+    $(test $UID = 0 && echo /sbin /usr/sbin /usr/local/sbin) \
+    ~/bin \
+    ; do
+    b_path_insert "$f"
+done
+unset f
+
+if test $UID = 0 -o "$USER" = cvs; then
+    export CVSREAD=true
+fi
+
+# Get most recent java and any jars in /usr/java
+for f in $(ls /usr/java/*.jar 2> /dev/null); do
+    export CLASSPATH=$CLASSPATH${CLASSPATH+:}$f
+done
+unset f
+
+if [ -d $HOME/src/java ]; then
+    export JAVA_ROOT=$HOME/src/java
+    export CLASSPATH="$CLASSPATH:$JAVA_ROOT"
+fi
+
+if [ -z "$PERLLIB" -a -d $HOME/src/perl ]; then
+    export PERLLIB=$HOME/src/perl
+fi
+export FTP_PASSIVE=1
+
+if test -f ~/.ssh/ssh_agent; then
+    . ~/.ssh/ssh_agent > /dev/null
+    if [ "$PS1" ]; then
+        if ps ${SSH_AGENT_PID-0} 2>&1 | grep ssh-agent > /dev/null 2>&1; then
+	    : We have a daemon
+	else
+	    # Start a daemon and add
+	    ssh-agent > ~/.ssh/ssh_agent
+	    . ~/.ssh/ssh_agent
+	    ssh-add
+            (x=~/.vagrant.d/insecure_private_key && test -f $x && ssh-add $x)
+	fi
+    fi
+fi
 
 if [ "x$TERM" = xdumb ]; then
     # Emacs needs a different version of dirs
--- bashrc.d/20-dev.sh
+++ bashrc.d/20-dev.sh
@@ -1,28 +1,55 @@
 # -*-sh-*-
-if [ $(expr match "$BASH_SOURCE" ~/src) == 0 -a -d ~/src/biviosoftware/home-env ]; then
+if [ $(expr match "$BASH_SOURCE" ~/src) = 0 -a -d ~/src/biviosoftware/home-env ]; then
     # Execute user's dot files only
     return
 fi
-export BIVIO_HTTPD_PORT=${BIVIO_HTTPD_PORT:-$(perl -e 'printf(q{80%02d}, (`id -u` =~ /(\d+)/)[0] % 100)')}
-export BIVIO_IS_2014STYLE=${BIVIO_IS_2014STYLE:-0}
 
-if [ -z "$BIVIO_HOST_NAME" -a "x$(hostname)" = xapa3.bivio.biz ]; then
-    export BIVIO_HOST_NAME=dev.bivio.biz
-fi
+function b_pyenv {
+    local v="$1"
+    export WORKON_HOME=$HOME/Envs
+    export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV=true
+    b_path_insert "$HOME/.pyenv/bin"
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+    pyenv virtualenvwrapper
+    if [ ! -z "$v" -a -d $WORKON_HOME ]; then
+	workon py$v
+	b_ps1 py$v
+    fi
+}
 
-if [ -z "$BIVIO_DEFAULT_BCONF" ]; then
-    for x in Artisans::BConf Bivio::DefaultBConf; do
-        if perl -M$x -e 1 2>/dev/null; then
-	    export BIVIO_DEFAULT_BCONF=$x
-	    break
-        fi
-    done
-fi
-
-if [ ! -z "$BIVIO_DEFAULT_BCONF" ]; then
-    eval "$(env BCONF=$BIVIO_DEFAULT_BCONF bivio dev bashrc_b_env_aliases)"
-    b_env pet Bivio/PetShop && cd - > /dev/null
-fi
+function b_install_pyenv {
+    local v="$1"
+    local vv
+    case "x$v" in
+	x3)
+	    vv=3.4.2
+	    ;;
+	x2)
+	    vv=2.7.8
+	    ;;
+	*)
+	    echo 'You need to supply a python version' 1>&2
+	    return 1
+	    ;;
+    esac
+    if [ ! -d ~/.pyenv ]; then
+	curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+    fi
+    b_pyenv $v 2>/dev/null
+    pyenv install $vv
+    if [ $v = 3 ]; then
+	pyenv global $vv
+    fi
+    pip install virtualenvwrapper
+    if [ ! -d ~/.pyenv/plugins/pyenv-virtualenvwrapper ]; then
+	git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
+    fi
+    b_pyenv $v 2>/dev/null
+    pyenv virtualenvwrapper
+    mkvirtualenv py$v
+    b_pyenv $v
+}
 
 function gcl {
     local r=$1
@@ -52,9 +79,32 @@ function ctd {
 EOF
 }
 
+export BIVIO_HTTPD_PORT=${BIVIO_HTTPD_PORT:-$(perl -e 'printf(q{80%02d}, (`id -u` =~ /(\d+)/)[0] % 100)')}
+export BIVIO_IS_2014STYLE=${BIVIO_IS_2014STYLE:-0}
+
+if [ -z "$BIVIO_HOST_NAME" -a "x$(hostname)" = xapa3.bivio.biz ]; then
+    export BIVIO_HOST_NAME=dev.bivio.biz
+fi
+
+if [ -z "$BIVIO_DEFAULT_BCONF" ]; then
+    for x in Artisans::BConf Bivio::DefaultBConf; do
+        if perl -M$x -e 1 2>/dev/null; then
+	    export BIVIO_DEFAULT_BCONF=$x
+	    break
+        fi
+    done
+fi
+
+if [ ! -z "$BIVIO_DEFAULT_BCONF" ]; then
+    eval "$(env BCONF=$BIVIO_DEFAULT_BCONF bivio dev bashrc_b_env_aliases)"
+    b_env pet Bivio/PetShop && cd - > /dev/null
+fi
+
 alias gup='git pull'
 alias gpu='git push origin master'
 alias gco='git commit -am'
 alias gst='git status'
 alias nup="cvs -n up 2>/dev/null|egrep '^[A-Z] |^\\? .*\\.(pm|bview|gif|jpg|t|PL|btest|bunit|bconf|msg|css|js|png|psd|pdf|spec|xml|java)$'"
 alias up="cvs up -Pd"
+alias py3='b_pyenv 3'
+alias py2='b_pyenv 2'
