019b7f36a14b448dfb9c212970d2f8e966de069d
robnagler <github@q33.us>
2014-12-10 00:01:11 +0000

    made robust against existing files added .netrc and .gitconfig

--- install.sh
+++ install.sh
@@ -1,23 +1,42 @@
 #!/bin/bash
 # curl -L https://raw.githubusercontent.com/biviosoftware/home-env/master/bin/install.sh | bash
-h=~/src/biviosoftware/home-env 
-if [ -d "$h" ]; then
-    echo "$h exists; remove first:" 1>&2
-    echo "rm -rf $h" 1>&2
-    exit 1
+biviosoftware=~/src/biviosoftware
+if [ ! -d $biviosoftware ]; then
+    mkdir -p $biviosoftware
 fi
-mkdir -p "$(dirname $h)" 2>/dev/null
-cd $(dirname $h)
-git clone https://github.com/biviosoftware/home-env
-git clone https://github.com/biviosoftware/perl-Bivio
-mkdir ../perl
+cd $biviosoftware
+for repo in home-env perl-Bivio javascript-Bivio perl-ProjEx; do
+    if [ -d $repo ]; then
+	echo "Using existing $repo"
+    else
+        git clone https://github.com/biviosoftware/$repo
+    fi
+done
+if [ ! -d ../perl ]; then
+    mkdir ../perl
+fi
+if [ -L ../perl/Bivio ]; then
+    rm ../perl/Bivio
+fi
 ln -s ../biviosoftware/perl-Bivio ../perl/Bivio
-cd $h
-for b in $(perl -e 'print(map(/^dot-(\w+)$/ ? "$1\n" : (), glob("dot-*")))'); do
-    src="$h/dot-$b"
-    dst=~/.$b
+cd home-env
+for dotfile in $(perl -e 'print(map(/^dot-(\w+)$/ ? "$1\n" : (), glob("dot-*")))'); do
+    src="$PWD/dot-$dotfile"
+    dst=~/.$dotfile
     rm -f "$dst.old"
-    test -e "$dst" && mv "$dst" "$dst.old"
-    rm -f "$dst"
+    if [ -e "$dst" -o -L "$dst" ]; then
+	mv "$dst" "$dst.old"
+    fi
     ln -s "$src" "$dst"
+done
+cd
+for dotfile in .gitconfig .netrc; do
+    if [ ! -r $dotfile ]; then
+	src=/my/$dotfile
+	if [ -r $src ]; then
+	    install --mode=0600 $src $dotfile
+	else
+	    echo "You need to install $dotfile"
+	fi
+    fi
 done
