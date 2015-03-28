7cbc57ca8c2a662ff445789455cbf82cab392bb0
robnagler <github@q33.us>
2015-03-28 15:49:27 -0600

    fix sorting of from

--- emacs.d/b-python.el
+++ emacs.d/b-python.el
@@ -96,8 +96,8 @@
 :license: " b-python-license "
 \"\"\"
 
-from __future__ import print_function
 from __future__ import absolute_import
+from __future__ import print_function
 ")
   (goto-char (point-min))
   (re-search-forward "\\?"))
