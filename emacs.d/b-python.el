b2e6fb8674219d4e1d0bb9a679ce6d0d7417abd9
robnagler <github@q33.us>
2016-09-15 15:32:10 +0000

    default to py.test not pykern pytest

--- emacs.d/b-python.el
+++ emacs.d/b-python.el
@@ -12,7 +12,7 @@
   "Returns a string compile-command."
   (concat
    (cond
-    ((string-equal "pytest" (b-python-file-type)) "pykern pytest ")
+    ((string-equal "pytest" (b-python-file-type)) "py.test ")
     (t "python "))
    (if (buffer-file-name)
        (file-name-nondirectory (buffer-file-name))
