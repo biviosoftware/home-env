681d21fcc372524eed24bf4ce4b484d701a09d5c
robnagler <github@q33.us>
2017-08-10 07:28:13 +0000

    bug in directory test

--- bashrc.d/zz-10-base.sh
+++ bashrc.d/zz-10-base.sh
@@ -99,7 +99,7 @@ if bivio class info Bivio::BConf >& /dev/null; then
         bivio "$@"
     }
 
-    if [[ ~/src/biviosofware/perl-Bivio ]]; then
+    if [[ -d ~/src/biviosofware/perl-Bivio ]]; then
         bu() {
             bivio test unit "${@-.}"
         }
