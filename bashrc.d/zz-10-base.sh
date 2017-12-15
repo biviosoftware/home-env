82fffcc4723496e1bdfd4cf21b84c5fb0d5c5f62
robnagler <github@q33.us>
2017-12-15 21:48:33 +0000

    BIVIO_WANT_PERL only if GMP is there

--- bashrc.d/zz-10-base.sh
+++ bashrc.d/zz-10-base.sh
@@ -187,7 +187,10 @@ fi
 
 #POSIT: duplicate code in install.sh
 if [[ -z ${BIVIO_WANT_PERL+x} ]]; then
-    if [[ -n ${BIVIO_HTTPD_PORT+x} || -n ${BIVIO_HOST_NAME+x} ||  -e /etc/centos-release ]]; then
+    # If someone set these vars, they expect to do perl development
+    # or only on centos if GMP for perl is installed (only if BOP is installed).
+    if [[ -n ${BIVIO_HTTPD_PORT+x} || -n ${BIVIO_HOST_NAME+x}
+       || (-e /etc/centos-release && $(perl -MGMP::Mpf -e 1 2>&1) == '') ]]; then
         export BIVIO_WANT_PERL=1
     else
         export BIVIO_WANT_PERL=
