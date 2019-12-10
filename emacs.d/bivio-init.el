9ed7d4ea3fe411e0afc71dd143c4a8878f2bdcc0
robnagler <github@q33.us>
2019-12-10 16:21:19 +0000

    xterm-color is problematic
    Entering a container with `ls` aliased to `ls --color`, ls seems to hang,
    and there is no output. When running mysql, gets:
    `error in process filter: if: Wrong number of arguments: <=, 3`

--- emacs.d/bivio-init.el
+++ emacs.d/bivio-init.el
@@ -14,7 +14,7 @@
 (require 'shell)
 (require 'mmm-mode)
 (require 'uniquify)
-(require 'xterm-color)
+;;(require 'xterm-color)
 ;;; remove after emacs 23 is gone
 (ignore-errors (require 'web-mode))
 (ignore-errors (require 'espresso))
@@ -161,14 +161,14 @@
 (setq comint-output-filter-functions
       (remove 'ansi-color-process-output comint-output-filter-functions))
 
-(add-hook 'shell-mode-hook
-          (lambda ()
-            ;; Disable font-locking in this buffer to improve performance
-            (font-lock-mode -1)
-            ;; Prevent font-locking from being re-enabled in this buffer
-            (make-local-variable 'font-lock-function)
-            (setq font-lock-function (lambda (_) nil))
-            (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter nil t)))
+;(add-hook 'shell-mode-hook
+;          (lambda ()
+;            ;; Disable font-locking in this buffer to improve performance
+;            (font-lock-mode -1)
+;            ;; Prevent font-locking from being re-enabled in this buffer
+;            (make-local-variable 'font-lock-function)
+;            (setq font-lock-function (lambda (_) nil))
+;            (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter nil t)))
 
 (if (and (getenv "BCONF")
          (or (getenv "BIVIO_HTTPD_PORT")
