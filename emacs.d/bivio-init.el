2cd43e88624c126aad13c89961ea637d0d98f8c4
robnagler <github@q33.us>
2023-06-28 09:44:43 -0600

    ckp

--- emacs.d/bivio-init.el
+++ emacs.d/bivio-init.el
@@ -75,6 +75,20 @@
 
 (add-to-list 'compilation-error-regexp-alist
 	     '(".*at \\([^ ]+\\) line \\([0-9]+\\)\\.?\n" 1 2))
+(defun bivio-shell-mode-hook (&optional arg)
+  (setq
+   shell-dirstack-query "command dirs"
+   explicit-shell-file-name shell-file-name)
+  (add-hook 'window-configuration-change-hook 'b-comint-fix-window-size nil t)
+; stuff not for everybody
+;            ;; Disable font-locking in this buffer to improve performance
+;            (font-lock-mode -1)
+;            ;; Prevent font-locking from being re-enabled in this buffer
+;            (make-local-variable 'font-lock-function)
+;            (setq font-lock-function (lambda (_) nil))
+;            (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter nil t)))
+  (define-key shell-mode-map "\C-cc" 'dirs))
+
 (if (file-readable-p "/bin/bash")
     (progn
       (setq
@@ -82,14 +96,7 @@
        shell-command-switch "-lc")
       (require 'bash-completion)
       (bash-completion-setup)
-      (add-hook
-       'shell-mode-hook
-       (lambda (&optional arg)
-         (setq
-          shell-dirstack-query "command dirs"
-          explicit-shell-file-name shell-file-name)
-         (add-hook 'window-configuration-change-hook 'b-comint-fix-window-size nil t)
-         (define-key shell-mode-map "\C-cc" 'dirs)))))
+      (add-hook 'shell-mode-hook 'bivio-shell-mode-hook)))
 
 ;;; https://github.com/syl20bnr/spacemacs/issues/6820#issuecomment-239665146
 ;;; keep same window for starting shells or listing buffers
@@ -195,15 +202,6 @@
 
 (setq comint-output-filter-functions
       (remove 'ansi-color-process-output comint-output-filter-functions))
-
-;(add-hook 'shell-mode-hook
-;          (lambda ()
-;            ;; Disable font-locking in this buffer to improve performance
-;            (font-lock-mode -1)
-;            ;; Prevent font-locking from being re-enabled in this buffer
-;            (make-local-variable 'font-lock-function)
-;            (setq font-lock-function (lambda (_) nil))
-;            (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter nil t)))
 
 (if (and (getenv "BCONF")
          (or (getenv "BIVIO_HTTPD_PORT")
